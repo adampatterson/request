07a7da04de250a99757a4c4ae8a7fe0789118bcd
Adam Patterson <hello@adampatterson.ca>
2026-04-24 17:42:45 -0600

    Converted tests to static accessors through the Request facade

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -19,14 +19,14 @@ class RequestTest extends TestCase
     }
 
     #[Test]
-    public function it_returns_the_instance()
+    public function it_returns_the_instance(): void
     {
         $request = Request::new();
-        $this->assertInstanceOf(Symfony\Component\HttpFoundation\Request::class, $request->instance());
+        $this->assertInstanceOf(Symfony\Component\HttpFoundation\Request::class, Request::instance());
     }
 
     #[Test]
-    public function it_can_be_called_statically()
+    public function it_can_be_called_statically(): void
     {
         $_SERVER['REQUEST_METHOD'] = 'POST';
 
@@ -34,16 +34,15 @@ class RequestTest extends TestCase
     }
 
     #[Test]
-    public function it_returns_the_request_method()
+    public function it_returns_the_request_method(): void
     {
         $_SERVER['REQUEST_METHOD'] = 'PUT';
 
-        $request = MakeRequest::new();
-        $this->assertEquals('PUT', $request->method());
+        $this->assertEquals('PUT', Request::method());
     }
 
     #[Test]
-    public function it_returns_the_root()
+    public function it_returns_the_root(): void
     {
         $_SERVER['HTTP_HOST'] = 'example.com';
         $_SERVER['PHP_SELF'] = '/index.php';
@@ -52,7 +51,7 @@ class RequestTest extends TestCase
     }
 
     #[Test]
-    public function it_returns_the_uri()
+    public function it_returns_the_uri(): void
     {
         $_SERVER['HTTP_HOST'] = 'example.com';
         $_SERVER['REQUEST_URI'] = '/test/path?query=1';
@@ -62,38 +61,33 @@ class RequestTest extends TestCase
     }
 
     #[Test]
-    public function it_returns_client_ip()
+    public function it_returns_client_ip(): void
     {
         $_SERVER['REMOTE_ADDR'] = '192.168.1.100';
-
-        $request = MakeRequest::new();
-        $this->assertEquals('192.168.1.100', $request->ip());
+        $this->assertEquals('192.168.1.100', Request::ip());
     }
 
     #[Test]
-    public function it_returns_user_agent()
+    public function it_returns_user_agent(): void
     {
         $_SERVER['HTTP_USER_AGENT'] = 'PHPUnit Test Agent';
 
-        $request = MakeRequest::new();
-        $this->assertEquals('PHPUnit Test Agent', $request->userAgent());
+        $this->assertEquals('PHPUnit Test Agent', Request::userAgent());
     }
 
     #[Test]
-    public function it_can_get_a_value()
+    public function it_can_get_a_value(): void
     {
         $_GET['foo'] = 'bar';
         $_POST['test'] = 'value'; // Symfony request get() checks query, request, etc.
 
-        $request = MakeRequest::new();
-
-        $this->assertEquals('bar', $request->get('foo'));
-        $this->assertEquals('value', $request->get('test'));
-        $this->assertEquals('default', $request->get('missing', 'default'));
+        $this->assertEquals('bar', Request::get('foo'));
+        $this->assertEquals('value', Request::get('test'));
+        $this->assertEquals('default', Request::get('missing', 'default'));
     }
 
     #[Test]
-    public function it_can_get_all_query_values()
+    public function it_can_get_all_query_values(): void
     {
         $_GET = [
             'foo' => 'bar',
@@ -101,20 +95,48 @@ class RequestTest extends TestCase
         ];
 
         // MakeRequest->all() specifically calls $this->request->query->all() which maps to $_GET
-        $request = MakeRequest::new();
-
-        $this->assertEquals($_GET, $request->all());
+        $this->assertEquals($_GET, Request::all());
     }
 
     #[Test]
-    public function it_proxies_method_calls_to_the_underlying_request()
+    public function it_can_get_all_post_values(): void
+    {
+        $_POST = [
+            'foo' => 'bar',
+            'baz' => 'qux',
+        ];
+
+        $this->assertEquals($_POST, Request::all('POST'));
+        $this->assertEquals($_POST, Request::all('post'));
+    }
+
+    #[Test]
+    public function it_can_get_all_cookie_values(): void
+    {
+        $_COOKIE = [
+            'session_id' => '12345',
+        ];
+
+        $this->assertEquals($_COOKIE, Request::all('COOKIE'));
+        $this->assertEquals($_COOKIE, Request::all('cookie'));
+    }
+
+    #[Test]
+    public function it_can_check_if_a_key_exists(): void
+    {
+        $_GET['foo'] = 'bar';
+
+        $this->assertTrue(Request::has('foo'));
+        $this->assertFalse(Request::has('bar'));
+    }
+
+    #[Test]
+    public function it_proxies_method_calls_to_the_underlying_request(): void
     {
         $_SERVER['REQUEST_URI'] = '/test-path';
 
-        $request = MakeRequest::new();
-
         // getPathInfo() is a method on Symfony's Request, but not on MakeRequest
-        $this->assertEquals('/test-path', $request->getPathInfo());
+        $this->assertEquals('/test-path', Request::getPathInfo());
         $this->assertEquals('/test-path', Request::getPathInfo());
     }
 }
